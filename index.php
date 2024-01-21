94378be8bbfdcf447e30a4f5fa7ed83b646f986e
philemmons <pemmons@csumb.edu>
2024-01-20 18:18:52 -0800

    css navbars

--- index.php
+++ index.php
@@ -31,15 +31,7 @@ include_once 'header.inc'
         <li class="nav-item">
           <a class="nav-link" href="contact.php">Contact Us</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="">Link</a>
-        </li>
-        
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
   </div>
 </nav>
