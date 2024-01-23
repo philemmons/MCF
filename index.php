af50e687e4cf2c76a9f3366a04e6ae6ada69e774
philemmons <pemmons@csumb.edu>
2024-01-23 15:52:13 -0800

    add card

--- index.php
+++ index.php
@@ -82,6 +82,25 @@ include_once 'header.inc'
     </div>
   </section>
 
+  <section>
+    <div class="container-fluid" style="background-color: var(--color-11);">
+      <div class="card mb-3">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="..." class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
 
   <?php include_once 'footer.inc' ?>
 </body>
