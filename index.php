00de384decd4c4670ff40fc0173626dd5b6c2ff7
philemmons <pemmons@csumb.edu>
2024-01-22 20:48:17 -0800

    change the font and spacing

--- index.php
+++ index.php
@@ -47,8 +47,8 @@ include_once 'header.inc'
     <div class="row">
       <div class="col-md-6 banner">
         <h1 class="header-font">Millie's Crazy Flowers</h1>
-        <h4 class= "font-italic">Legacy of love, caring, and lasting friendship from one friend to another.</>
-        <p> Our Mogol Artist hand crafts every beautiful flower that no two are alike. </p> 
+        <h4 class= "font-italic">Legacy of love, caring, and lasting friendship.</h4>
+        <p class= "py-2"> Our Mogol Artist hand crafts every beautiful flower that no two are alike. </p> 
       </div>
 
       <div class="col-md-6 banner text-center">
