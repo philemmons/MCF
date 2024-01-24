dc78f83f2438a1fce3a37654551c064e4aaa0d60
philemmons <pemmons@csumb.edu>
2024-01-24 09:39:01 -0800

    test2

--- index.php
+++ index.php
@@ -190,11 +190,11 @@ include_once 'header.inc'
         </div>
       </div>
 
-      <div id="carouselExampleAutoplaying" class="carousel slide" data-ride="carousel">
+      <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
           <div class="carousel-item active">
             <div class="cards-wrapper">
-              <div class="card d-none d-md-block">
+              <div class="card d-block">
                 <img src="..." class="card-img-top" alt="...">
                 <div class="card-body">
                   <h5 class="card-title">Card title 1</h5>
@@ -202,7 +202,7 @@ include_once 'header.inc'
                   <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
               </div>
-              <div class="card d-none d-md-block">
+              <div class="card d-block">
                 <img src="..." class="card-img-top" alt="...">
                 <div class="card-body">
                   <h5 class="card-title">Card title 2</h5>
@@ -210,7 +210,7 @@ include_once 'header.inc'
                   <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
               </div>
-              <div class="card d-none d-md-block">
+              <div class="card d-block">
                 <img src="..." class="card-img-top" alt="...">
                 <div class="card-body">
                   <h5 class="card-title">Card title 3</h5>
@@ -222,7 +222,7 @@ include_once 'header.inc'
           </div>
           <div class="carousel-item">
             <div class="cards-wrapper">
-              <div class="card d-none d-md-block">
+              <div class="card d-block">
                 <img src="..." class="card-img-top" alt="...">
                 <div class="card-body">
                   <h5 class="card-title">Card title 4</h5>
@@ -230,7 +230,7 @@ include_once 'header.inc'
                   <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
               </div>
-              <div class="card d-none d-md-block">
+              <div class="card d-block">
                 <img src="..." class="card-img-top" alt="...">
                 <div class="card-body">
                   <h5 class="card-title">Card title 5</h5>
@@ -238,7 +238,7 @@ include_once 'header.inc'
                   <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
               </div>
-              <div class="card d-none d-md-block">
+              <div class="card d-block">
                 <img src="..." class="card-img-top" alt="...">
                 <div class="card-body">
                   <h5 class="card-title">Card title 6</h5>
@@ -250,7 +250,7 @@ include_once 'header.inc'
           </div>
           <div class="carousel-item">
             <div class="cards-wrapper">
-              <div class="card d-none d-md-block">
+              <div class="card">
                 <img src="..." class="card-img-top" alt="...">
                 <div class="card-body">
                   <h5 class="card-title">Card title 7</h5>
@@ -258,7 +258,7 @@ include_once 'header.inc'
                   <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
               </div>
-              <div class="card d-none d-md-block">
+              <div class="card">
                 <img src="..." class="card-img-top" alt="...">
                 <div class="card-body">
                   <h5 class="card-title">Card title 8</h5>
@@ -266,7 +266,7 @@ include_once 'header.inc'
                   <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
               </div>
-              <div class="card d-none d-md-block">
+              <div class="card">
                 <img src="..." class="card-img-top" alt="...">
                 <div class="card-body">
                   <h5 class="card-title">Card title 9</h5>
