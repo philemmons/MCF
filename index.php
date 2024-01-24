6cd21324e886483a20600ef673627b79a2c2653c
philemmons <pemmons@csumb.edu>
2024-01-24 14:33:22 -0800

    lets see what it looks like wiout the MVC

--- index.php
+++ index.php
@@ -389,10 +389,9 @@ include_once 'header.inc'
 
 
 
-
-
+  
   <section class="px-3 px-lg-5 pt-3 pt-lg-5">
-    <div class="container-fluid">
+    <div class="container-">
       <div class="row text-center">
         <div class="col-lg-12 my-2 my-lg-3 pt-5">
           <div class="card-body">
@@ -417,6 +416,52 @@ include_once 'header.inc'
       </div>
     </div>
   </section>
+
+  <div class="row justify-content-center g-0">
+    <div class="col-md-4 text-center d-flex align-items-stretch aos-init aos-animate" data-aos="fade-up" data-aos-delay="100" data-aos-duration="1000">
+      <div class="flow-wrap flow-wrap-right">
+        <div class="icon">
+          <div class="d-flex align-items-center justify-content-center"><small>01</small></div>
+          <span class="flaticon-calculator"></span>
+        </div>
+        <div class="text">
+          <h2>Get A Free Quote</h2>
+          <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 text-center d-flex align-items-stretch aos-init aos-animate" data-aos="fade-up" data-aos-delay="100" data-aos-duration="1000">
+      <div class="flow-wrap flow-wrap-2">
+        <div class="icon">
+          <div class="d-flex align-items-center justify-content-center"><small>02</small></div>
+          <span class="flaticon-production"></span>
+        </div>
+        <div class="text">
+          <h2>Production</h2>
+          <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 text-center d-flex align-items-stretch aos-init aos-animate" data-aos="fade-up" data-aos-delay="100" data-aos-duration="1000">
+      <div class="flow-wrap flow-wrap-left">
+        <div class="icon">
+          <div class="d-flex align-items-center justify-content-center"><small>03</small></div>
+          <span class="flaticon-package"></span>
+        </div>
+        <div class="text">
+          <h2>Delivery &amp; Assembly</h2>
+          <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-12 text-center aos-init aos-animate" data-aos="fade-up" data-aos-duration="1000">
+      <p><a href="#" class="btn btn-primary py-3 px-4">Learn More</a> <a href="#" class="btn btn-secondary py-3 px-4">Get A Request</a></p>
+    </div>
+  </div>
+
+
+
+
 
 
   <section class="px-3 px-lg-5 pt-3 pt-lg-5" style="background-color: var(--color-1);">
