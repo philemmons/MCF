81085df7971fe045114f1df6e01f475801031e78
philemmons <pemmons@csumb.edu>
2024-01-26 11:20:46 -0800

    more css

--- contact.php
+++ contact.php
@@ -40,10 +40,10 @@ include_once 'header.inc'
 
     <!-- Hero Section -->
     <section class="container-fluid">
-        <div class="row justify-content-center align-items-center py-6 mb-5 bg-contactUs" title="We are here to answer your questions and welcome your feedback.">
-            <div class="col-xl-7 col-lg-7 col-md-12 py-5">
-                <div class="p-3 text-center banner" title="Contact Us">
-                    <h1 class="header-font mb-3 px-3 px-md-0">Contact Us
+        <div class="row justify-content-center align-items-end mb-5 bg-contactUs" title="We are here to answer your questions and welcome your feedback.">
+            <div class="col-xl-6 col-lg-6 col-md-12">
+                <div class="text-center banner" title="Contact Us">
+                    <h1 class="header-font mb-5 py-4 px-3 px-md-0">Contact Us
                     </h1>
                 </div>
             </div>
