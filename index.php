36d3e5235dcfc35c99f31291ab20b630a53dedde
philemmons <pemmons@csumb.edu>
2024-01-25 16:32:30 -0800

    tooltips?

--- index.php
+++ index.php
@@ -120,7 +120,7 @@ include_once 'header.inc'
 
       <div class="row text-center">
         <div class="col-lg-4 p-5 info-4 border border-white">
-          <img src="images/mcf-flower-1.png" class="img-fluid l-box" alt="MCF's Chenille Stems flower one.">
+          <img src="images/mcf-flower-1.png" class="img-fluid l-box" alt="MCF's Chenille Stems flower one." data-bs-toggle="tooltip" data-bs-title="Another one here too">
         </div>
 
         <div class="col-lg-4 p-5 info-4 border border-white">
