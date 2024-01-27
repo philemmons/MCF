322c5b3e043db8cde378744e6dfe409f546cfc92
philemmons <pemmons@csumb.edu>
2024-01-26 22:58:36 -0800

    font reduction

--- contact.php
+++ contact.php
@@ -41,8 +41,8 @@ include_once 'header.inc'
         <!-- Hero Section -->
         <section class="container-fluid">
             <div class="row justify-content-center align-items-end mb-5 bg-contactUs" title="We are here to answer your questions and welcome your feedback.">
-            <div class="col-xl-6 col-lg-7 col-md-9">
-                        <div class="text-center">
+                <div class="col-xl-6 col-lg-7 col-md-9">
+                    <div class="text-center">
                         <p class="fw-bold ">
                             <a href="index.php" class="mcf-crumb">HOME&gt;</a>&nbsp;&nbsp;<a href="#" class="mcf-crumb">CONTACT US&gt;</a>
                         </p>
@@ -61,7 +61,7 @@ include_once 'header.inc'
             <div class="row justify-content-center">
                 <div class="col-xl-10 col-lg-10 col-md-12 py-4">
                     <div class="p-3 text-center text-bg-light hero-text-border">
-                        <h3 class="card-title mb-3">We operate out of Valley Springs, California.</h3>
+                        <p class="card-title mb-3 h4">We operate out of Valley Springs, California.</p>
                         <div class="col-md-12 db-5">
                             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25088.668857840803!2d-120.83613645418204!3d38.184731983234784!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80908114b17cbaf5%3A0x2a7cf4cabd8c124d!2sValley%20Springs%2C%20CA%2095252!5e0!3m2!1sen!2sus!4v1706303237454!5m2!1sen!2sus" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                         </div>
