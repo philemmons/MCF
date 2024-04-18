0a86a49fa59db1c0b860a102682ffb2f212cb446
philemmons <pemmons@csumb.edu>
2024-04-18 14:01:11 -0700

    code check in

--- contact.php
+++ contact.php
@@ -247,7 +247,7 @@ include_once 'header.inc'
                             </div>
 
                             <div class="col-md-6 text-center">
-                                <button type="reset" class="btn btn-primary" name="reset" value="reset" onclick="return resetFields();" aria-labelledby="reset">Reset Form</button>
+                                <button type="reset" class="btn mcf-button" name="reset" value="reset" onclick="return resetFields();" aria-labelledby="reset">Reset Form</button>
                                 <div class="sr-only" id="reset" role="alert" aria-live="assertive" aria-atomic="true">
                                     <p>(A pop up will confirm your reset)</p>
                                 </div>
