304af2dcef83e22195c1a8f3d7f80eb046600579
philemmons <pemmons@csumb.edu>
2024-04-26 09:33:33 -0700

    one more time

--- contact.php
+++ contact.php
@@ -245,7 +245,7 @@ include_once 'header.inc'
                         </div>
                     </div>
 
-                    <div class="col-xl-8 col-lg-8 col-md-12 pt-5">
+                    <div class="col-xl-8 col-lg-8 col-md-12 mb-5">
                         <div class="p-3 text-center hero-text-border banner" title="Please contact us with any questions, suggestions, or concerns.">
                             <section aria-label="Talk to Us">
                                 <h2 class="h5 mb-6 px-3 px-md-0">Please allow us up to 48 hours to respond, and if you need assistance sooner, please email <?php echo getenv('mcf-to-email'); ?>
