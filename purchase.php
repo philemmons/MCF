95e301e3e33a8c9eeca661a9a08cdbb4c7af589e
philemmons <pemmons@csumb.edu>
2024-07-04 15:57:40 -0700

    layout

--- purchase.php
+++ purchase.php
@@ -71,7 +71,7 @@ include_once 'header-bottom.inc'
                     <div class="col-xl-10 col-lg-10 col-md-12 pt-4">
                         <div class="p-3  text-bg-light hero-text-border" title="Support, Happiness, and Memories.">
                             <section aria-label="Purchase-Intro">
-                                <h2 class="card-title bswash-font mb-3">Millie's Crazy Flowers' Order Form<br>4th of July Flash Sale 10% Off!</h2>
+                                <h2 class="card-title bswash-font mb-3">Millie's Crazy Flowers' Order Form - 4th of July Flash Sale 10% Off!</h2>
                                 <p>We are grateful you have decided to purchase, one or more, Millie's Crazy Flowers. Thank you for supporting our communities most vulnerable, promoting happiness, and taking action! 50% of our proceeds will be donated between <a href="https://soor.org/give/" class="mcf-link  fw-bold" target="_blank" rel="noopener noreferrer">Special Olympics Oregon<span class="sr-only">(Opens a new window)</span></a> and <a href="https://www.crossingbridgestrc.org/" class="mcf-link  fw-bold" target="_blank" rel="noopener noreferrer">Crossing Bridges Therapeutic Riding Center<span class="sr-only">(Opens a new window)</span></a>.</p>
                                 <p>Each flower is $12.50, or buy TWO and get ONE free. Postage and handling for three flowers is $12.00, or $6.00 each. We'll send you an email with your order and amount due. Please allow for up to 48 hours, once your payment has been received, for us to mail your flowers with USPS.</p>
                                 <p>4th of July Flash Sale 10% Off! - You will see your saving in the email confirmation!</p>
